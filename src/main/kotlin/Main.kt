2666fd3788415922f297b63c3f1de9eb07e4f695
Nourez Rawji <nourez@nourez.io>
2019-05-16 10:22:52 -0400

    Tutorial started

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
-import starter.gameLoop
+//import starter.gameLoop
+import screeps.api.*
 
 /**
  * Entry point
@@ -8,5 +9,19 @@ import starter.gameLoop
  */
 @Suppress("unused")
 fun loop() {
-    gameLoop()
+    val creep = Game.creeps["Harvester1"]
+    val spawn = Game.spawns["Spawn1"]
+
+    if (creep != null && spawn != null) { //null safety check
+        if (creep.carry.energy < creep.carryCapacity) {
+            val sources = creep.room.find(FIND_SOURCES)
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0])
+            }
+        } else {
+            if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(spawn)
+            }
+        }
+    } //null safety check
 }
